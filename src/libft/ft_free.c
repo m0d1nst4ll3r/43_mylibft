5d00cd081b0b2817908c4f66597054ccb49f8494
Raphael Pohlen <rapohlen@f2r11s15.clusters.42paris.fr>
2026-04-22 13:40:11 +0200

    ft_free removed conditional, now just sets pointer to NULL

--- src/libft/ft_free.c
+++ src/libft/ft_free.c
@@ -6,20 +6,14 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 15:35:51 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/01/15 15:39:41 by rapohlen         ###   ########.fr       */
+/*   Updated: 2026/04/22 13:39:23 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Better free, does not syscall if address is already NULL and sets to NULL
 void	ft_free(void **p)
 {
-	if (!p)
-		return ;
-	if (*p)
-	{
-		free(*p);
-		*p = NULL;
-	}
+	free(*p);
+	*p = NULL;
 }
